0e3acd305c66ace914f650437550bc8337e57984
Mike M <mikal815@users.noreply.github.com>
2018-11-03 12:44:38 -0400

    Remove Yarn

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,7 +3,7 @@ import "./Banner.css";
 
 const Banner = () => (
     <div className="banner text-center">
-        <h1>Clicky Game</h1>
+        <h1>The Simpsons Clicky Game</h1>
         <p>Click on an image to earn points, but don't click on any image more than once or the game ends!</p>
     </div>
 )
