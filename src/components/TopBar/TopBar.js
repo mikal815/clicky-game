d165244a3ac97dfa61e91ab179a354f62725c9b3
Mike M <mikal815@users.noreply.github.com>
2019-04-06 21:42:37 -0400

    Update images

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -4,7 +4,7 @@ import "./TopBar.css";
 const TopBar = props => (
     <nav className="navbar navbar-default navbar-fixed-top">
         <div className="container">
-            <h3 className="navbar-brand">Clicky Game</h3>
+            <h3 className="navbar-brand">Shape Shifter</h3>
             <div className="text-center">
                 <h4>{props.message}</h4>
             </div>
