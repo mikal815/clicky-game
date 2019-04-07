fcda2c42f9c301c8686b0b841a2b7a417d30dc38
Mike M <mikal815@users.noreply.github.com>
2019-04-07 12:15:43 -0400

    Fix image problem

--- src/components/GameBoard/GameBoard.js
+++ src/components/GameBoard/GameBoard.js
@@ -2,15 +2,19 @@ import React from "react";
 import "./GameBoard.css";
 
 const GameBoard = props => (
-    <div className="row">
-        <div className="col-md-2">
+    
+        <div className="row">
+            
+            <div className="col-md-2">
+            </div>
+            <div className="col-md-8">
+                {props.children}
+            </div>
+            <div className="col-md-2">
+            </div>
+           
         </div>
-        <div className="col-md-8">
-            {props.children}
-        </div>
-        <div className="col-md-2">
-        </div>
-    </div>
+    
 )
 
 export default GameBoard;
